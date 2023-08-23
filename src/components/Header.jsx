5fcc87e514bf4388b6acb7c5df01b7c1a24bba66
yashverma03 <vermayash2003@gmail.com>
2023-08-23 17:13:50 +0530

    updated header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import CloudDoneOutlinedIcon from '@mui/icons-material/CloudDoneOutlined';
 const Header = () => {
   return (
     <div className='header-container'>
+      <div className="rectangle" />
       <h2 className='header-title'>Assignment Task</h2>
       <hr className='header-divider' />
 
@@ -16,7 +17,7 @@ const Header = () => {
           <CloudDoneOutlinedIcon />
         </div>
       </div>
-      
+
       <p className='header-description'>The name and photo associated with your Google account will be recorded when you upload files and submit this form. Your email is not part of your response.</p>
       <hr className='header-divider' />
       <p className='header-required'>* Indicates required question</p>
